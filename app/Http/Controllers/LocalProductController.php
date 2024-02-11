a96ac1a5f77d5160bb602713c33997e42a8db919
alyah0011 <alyah0011@gmail.com>
2024-02-11 16:39:42 +0800

    Filter change

--- app/Http/Controllers/LocalProductController.php
+++ app/Http/Controllers/LocalProductController.php
@@ -27,24 +27,34 @@ class LocalProductController extends Controller
     {
         try {
             $selectedCategories = $request->input('categories');
-
+    
             // Check if $selectedCategories is null, and set it to an empty array if it is
             $selectedCategories = $selectedCategories ?? [];
-
-            // Query products based on selected categories
-            $filteredProducts = LocalProduct::whereIn('lp_type', $selectedCategories)->get();
-
+    
+            // Add "Other" filter if selected
+            if (in_array('other', $selectedCategories)) {
+                // Define the excluded categories
+                $excludedCategories = ['Food', 'Fashion', 'Handicraft'];
+    
+                // Query products excluding the specified categories
+                $filteredProducts = LocalProduct::whereNotIn('lp_type', $excludedCategories)->get();
+            } else {
+                // Query products based on selected categories
+                $filteredProducts = LocalProduct::whereIn('lp_type', $selectedCategories)->get();
+            }
+            
+    
             // You can also return the filtered products as JSON if you prefer
             return response()->json(['html' => view('product.filter', ['products' => $filteredProducts])->render()]);
-
+    
         } catch (\Exception $e) {
             // Log the error
             \Log::error('Error in filterProducts: ' . $e->getMessage());
-
+    
             // Return a response with an error message (for debugging)
             return response()->json(['error' => $e->getMessage()], 500);
         }
-        
     }
+    
 
 }
